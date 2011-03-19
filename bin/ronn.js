0353e6cd0f612883319bea1739cdc8201749f415
TJ Holowaychuk <tj@vision-media.ca>
2011-03-19 17:43:55 +0100

    operate over stdio by default
    HATE when executables do not do this. I swear we used
    to have --pipe or something, but either way it seems gone
    now and I really need this functionality.

--- bin/ronn.js
+++ bin/ronn.js
@@ -6,7 +6,7 @@ var sys = require('sys');
 var fs = require('fs');
 var path = require('path');
 var opts = require('opts');
-var ronn = require(__dirname + '/../lib/ronn');
+var Ronn = require(__dirname + '/../lib/ronn').Ronn;
 
 var options = [
 	{ short       : 'V'
@@ -48,56 +48,67 @@ var options = [
 ];
 var arguments = [
 	{ name : 'file'
-	, required : true
+	, required : false
 	, description: 'A ronn file'
 	}
 ];
 opts.parse(options, arguments, true);
 
-var fPath = opts.arg('file');
-
-var fBase = path.join(path.dirname(fPath), path.basename(fPath, path.extname(fPath)));
-
-var fTxt = fs.readFileSync(fPath, 'utf8');
-var ronn = new ronn.Ronn(fTxt, opts.get("version"), opts.get("manual"), opts.get("date"));
-
-if (opts.get("man") && !opts.get("build")) {
-	var spawn = require('child_process').spawn;
-	var man = spawn('man', ['--warnings',  '-E UTF-8',  '-l',  '-'], {"LANG":"C"});
-	man.stdout.addListener('data', function (data) {
-		sys.puts(data);
-	});
-	man.stderr.addListener('data', function (data) {
-		sys.puts(data);
-	});
-	man.addListener('exit', function() {
-		process.exit(0);
-	});
-	man.stdin.write(ronn.roff(), 'utf8');
-	man.stdin.end();
+var buf = '';
+if (opts.arg('file')) {
+	var fPath = opts.arg('file');
+	var fBase = path.join(path.dirname(fPath), path.basename(fPath, path.extname(fPath)));
+	buf = fs.readFileSync(fPath, 'utf8');
+	render(buf);
 } else {
-	var fRoff = null;
-	var fHtml = null;
-	var fFrag = null;
-	if (!opts.get("html") && !opts.get("fragment")) fRoff = ronn.roff();
-	else {
-		if (opts.get("roff")) fRoff = ronn.roff();
-		if (opts.get("html")) fHtml = ronn.html();
-		if (opts.get("fragment")) {
-			if (opts.get("html")) {
-				sys.debug("Can't use both --fragment and --html");
-				process.exit(-1);
-			}
-			fFrag = ronn.fragment();
-		}
-	}
-	if (opts.get("build")) {
-		if (fRoff) fs.writeFileSync(fBase + ".roff", fRoff, 'utf8');
-		if (fHtml) fs.writeFileSync(fBase + ".html", fHtml, 'utf8');
-		if (fFrag) fs.writeFileSync(fBase + ".fragment", fFrag, 'utf8');
+	process.stdin
+		.on('data', function(chunk){ buf += chunk; })
+		.on('end', function(){
+			render(buf)
+		}).resume();
+}
+
+function render(pTxt) {
+	var ronn = new Ronn(pTxt, opts.get("version"), opts.get("manual"), opts.get("date"));
+
+	if (opts.get("man") && !opts.get("build")) {
+		var spawn = require('child_process').spawn;
+		var man = spawn('man', ['--warnings',  '-E UTF-8',  '-l',  '-'], {"LANG":"C"});
+		man.stdout.addListener('data', function (data) {
+			sys.puts(data);
+		});
+		man.stderr.addListener('data', function (data) {
+			sys.puts(data);
+		});
+		man.addListener('exit', function() {
+			process.exit(0);
+		});
+		man.stdin.write(ronn.roff(), 'utf8');
+		man.stdin.end();
 	} else {
-		if (fRoff) sys.puts(fRoff);
-		if (fHtml) sys.puts(fHtml);
-		if (fFrag) sys.puts(fFrag);
+		var fRoff = null;
+		var fHtml = null;
+		var fFrag = null;
+		if (!opts.get("html") && !opts.get("fragment")) fRoff = ronn.roff();
+		else {
+			if (opts.get("roff")) fRoff = ronn.roff();
+			if (opts.get("html")) fHtml = ronn.html();
+			if (opts.get("fragment")) {
+				if (opts.get("html")) {
+					sys.debug("Can't use both --fragment and --html");
+					process.exit(-1);
+				}
+				fFrag = ronn.fragment();
+			}
+		}
+		if (opts.get("build")) {
+			if (fRoff) fs.writeFileSync(fBase + ".roff", fRoff, 'utf8');
+			if (fHtml) fs.writeFileSync(fBase + ".html", fHtml, 'utf8');
+			if (fFrag) fs.writeFileSync(fBase + ".fragment", fFrag, 'utf8');
+		} else {
+			if (fRoff) sys.puts(fRoff);
+			if (fHtml) sys.puts(fHtml);
+			if (fFrag) sys.puts(fFrag);
+		}
 	}
 }
